868bf3e896af9bef8a3e8afaa7f8a7ba9451e635
W-Ox <w-oxley@outlook.com>
2026-05-01 11:22:42 +0100

    change thoughts to popup

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = Vue.createApp({
                 {ind:5,title:'Troy', author:'Stephen Fry', year:2026, showThoughts : false, thoughts : 'I really enjoyed this book, it was fairly difficult to start with as there are so many players and new names, and it jumps from side to side, but once the pieces are in place, it is a fantastic story and an easy read.', rating : 9},
                 {ind:6,title:'Odyssey', author:'Stephen Fry', year:2026, showThoughts : false, thoughts : 'A similar experience to that of "Troy", although slightly easier to read as the characters are already in place from that earlier entry in the series, I found the story so interesting along with the nature in which it is told. An excellent book.', rating : 10},
                 {ind:7,title:'The Princess Bride', author:'William Goldman', year:2026, showThoughts : false, thoughts : 'Just a fantastic story told in such a charming way.', rating : 9},
-                {ind:8,title:'As Leeds Go Marching On', author:'Jonny Cooper', year:2026, showThoughts : false, thoughts : 'Despite being based on stats, this book does an excellent job at telling the story of Leeds United through history, and it is done in a fun and light-hearted way.', rating : 7},
+                {ind:8,title:'As Leeds Go Marching On', author:'Jonny Cooper', year:2026, showThoughts : false, thoughts : 'Despite being base on stats, this book does an excellent job at telling the story of Leeds United through history, and it is done in a fun and light-hearted way.', rating : 7},
                 {ind:9,title:'Brief Answers To The Big Questions', author:'Stephen Hawking', year:2026, showThoughts : false, thoughts:'This books covers a range of topics and is presented in a way which is easy to read. The thoughts can get a bit repetitive especially with some questions being similar, though.', rating : 7},
                 {ind:10,title:'Enemy At The Gates', author:'William Craig', year: '-- In Progress --', showThoughts : false,  thoughts : 'Still in progress, but enjoying it so far.', rating : '-'},
                 {ind:11,title:'The Satsuma Complex', author:'Bob Mortimer', year: '-- Waiting List --', showThoughts : false, thoughts : 'n/a', rating : '-'},
@@ -21,24 +21,14 @@ const app = Vue.createApp({
             showingBook:-1
         }
     },
-    methods: {
-        toggleShowThoughts(b) {
-            if (this.showingBook===b.ind) {
-                b.showThoughts=false
-                this.showingBook=-1
-            }
-            else if (this.showingBook>0) {
-                this.books[this.showingBook].showThoughts=false
-                this.showingBook = b.ind
-                b.showThoughts = true
-            }
-            else {
-                this.showingBook = b.ind
-                b.showThoughts = true
-            }
-
-        }
-    }
+methods: {
+  openBook(book) {
+    this.showingBook = book.ind
+  },
+  closeBook() {
+    this.showingBook = -1
+  }
+}
 })
 
 app.mount('#app')
